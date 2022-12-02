0e70c95c3b0812afd6b2b63bdec594e086a838a0
Houn <xuxihong@minijoy.work>
2022-12-02 16:11:02 +0800

    fix

---
+++ TestWaterMark/app/src/main/java/cn/am7code/testwatermark/App.kt
@@ -0,0 +1,8 @@
+package cn.am7code.testwatermark
+
+import dagger.hilt.android.HiltAndroidApp
+import me.rosuh.easywatermark.WMApp
+
+@HiltAndroidApp
+class App:WMApp() {
+}
--- particle-connect/lib/particle_connect.dart
+++ particle-connect/lib/particle_connect.dart
@@ -15,14 +15,14 @@ class ParticleConnect {
   static const MethodChannel _channel = MethodChannel('connect_bridge');
 
   /// Init particle connect SDK.
-  /// 
+  ///
   /// [chainInfo] Chain info, for example EthereumChain, BscChain.
-  /// 
+  ///
   /// [dappMetaData] is dapp meta data, when connect with other wallets,
   /// will pass it to them.
-  /// 
+  ///
   /// [env] Development environment.
-  /// 
+  ///
   /// [RpcUrlConfig] set custom solana and evm rpc url, default is null.
   static Future<void> init(
       ChainInfo chainInfo,
@@ -48,15 +48,15 @@ class ParticleConnect {
             "chain_id_name": chainInfo.chainIdName,
             "chain_id": chainInfo.chainId,
             "env": env.name,
-            "metadata": dappMetaData,
-            "rpc_url": rpcUrlConfig
+            "metadata": metadataEntity,
+            "rpc_url": rpcUrlEntity
           }));
     }
   }
 
   /// Set chain info, update chain info to SDK.
   /// Call this method before login.
-  /// 
+  ///
   /// [chainInfo] Chain info, for example EthereumChain, BscChain.
   static Future<String> setChainInfo(ChainInfo chainInfo) async {
     return await _channel.invokeMethod(
@@ -69,20 +69,20 @@ class ParticleConnect {
   }
 
   /// Connect a wallet.
-  /// 
+  ///
   /// [walletType] is which wallet you want to connect.
-  /// 
+  ///
   /// Result account or error.
   static Future<String> connect(WalletType walletType) async {
     return await _channel.invokeMethod('connect', walletType.name);
   }
 
   /// Disconnect a wallet
-  /// 
+  ///
   /// [walletType] is which wallet you want to disconnect.
-  /// 
+  ///
   /// [publicAddress] is which public address you want to disconnect.
-  /// 
+  ///
   /// Result success or error.
   static Future<String> disconnect(
       WalletType walletType, String publicAddress) async {
@@ -95,9 +95,9 @@ class ParticleConnect {
   }
 
   /// IsConnected a wallet
-  /// 
+  ///
   /// [walletType] is which wallet you want to check.
-  /// 
+  ///
   /// [publicAddress] is which public address you want to check.
   static Future<bool> isConnected(
       WalletType walletType, String publicAddress) async {
@@ -110,19 +110,19 @@ class ParticleConnect {
   }
 
   /// Get accounts from specify wallet type.
-  /// 
+  ///
   /// [walletType] is which wallet you want to check.
-  /// 
+  ///
   /// Result accounts or error.
   static Future<String> getAccounts(WalletType walletType) async {
     return await _channel.invokeMethod('getAccounts', walletType.name);
   }
 
   /// Sign message.
-  /// 
+  ///
   /// Pass [walletType] and [publicAddress] to decide a wallet to sign the
   /// [message].
-  /// 
+  ///
   /// Return signature or error.
   static Future<String> signMessage(
       WalletType walletType, String publicAddress, String message) async {
@@ -136,10 +136,10 @@ class ParticleConnect {
   }
 
   /// Sign transaction, only support solana chain.
-  /// 
+  ///
   /// Pass [walletType] and [publicAddress] to decide a wallet to sign the
   /// [transaction].
-  /// 
+  ///
   /// Result signature or error.
   static Future<String> signTransaction(
       WalletType walletType, String publicAddress, String transaction) async {
@@ -153,10 +153,10 @@ class ParticleConnect {
   }
 
   /// Sign all transactions, only support evm chain.
-  /// 
+  ///
   /// Pass [walletType] and [publicAddress] to decide a wallet to sign the
   /// [transactions].
-  /// 
+  ///
   /// Result signatures or error.
   static Future<String> signAllTransactions(WalletType walletType,
       String publicAddress, List<String> transactions) async {
@@ -170,10 +170,10 @@ class ParticleConnect {
   }
 
   /// Sign and send transaction.
-  /// 
+  ///
   /// Pass [walletType] and [publicAddress] to decide a wallet to sign and send the
   /// [transaction].
-  /// 
+  ///
   /// Result signature or error.
   static Future<String> signAndSendTransaction(
       WalletType walletType, String publicAddress, String transaction) async {
@@ -187,12 +187,12 @@ class ParticleConnect {
   }
 
   /// Sign typed data, only support evm chain.
-  /// 
+  ///
   /// Pass [walletType] and [publicAddress] to decide a wallet to sign and send the
   /// [typedData].
-  /// 
+  ///
   /// [typedData] only support v4.
-  /// 
+  ///
   /// Result signatrue or error.
   static Future<String> signTypedData(
       WalletType walletType, String publicAddress, String typedData) async {
@@ -206,11 +206,11 @@ class ParticleConnect {
   }
 
   /// Import private key.
-  /// 
+  ///
   /// [walletType] pass solanaPrivateKey or evmPrivateKey.
-  /// 
+  ///
   /// [privateKey] is the private key.
-  /// 
+  ///
   /// Result account or error.
   static Future<String> importPrivateKey(
       WalletType walletType, String privateKey) async {
@@ -223,11 +223,11 @@ class ParticleConnect {
   }
 
   /// Import mnemonic
-  /// 
+  ///
   /// [walletType] pass solanaPrivateKey or evmPrivateKey.
-  /// 
+  ///
   /// [mnemonic] is mnemonic, for example "apple banana pear ...", 12 24 or 48 words.
-  /// 
+  ///
   /// Result account or error.
   static Future<String> importMnemonic(
       WalletType walletType, String mnemonic) async {
@@ -240,11 +240,11 @@ class ParticleConnect {
   }
 
   /// Export private key
-  /// 
+  ///
   /// [walletType] is which wallet you want to export private key, pass solanaPrivateKey or evmPrivateKey.
-  /// 
+  ///
   /// [publicAddress] is public address.
-  /// 
+  ///
   /// Result private key or error.
   static Future<String> exportPrivateKey(
       WalletType walletType, String publicAddress) async {
@@ -257,13 +257,13 @@ class ParticleConnect {
   }
 
   /// Sign in with Ethereum/Solana.
-  /// 
+  ///
   /// Pass [walletType] and [publicAddress] to decide a wallet.
   ///
   /// [domain] for example "particle.network"
-  /// 
+  ///
   /// [uri] for example "https://particle.network/demo#login"
-  /// 
+  ///
   /// Result source message and signature or error.
   static Future<String> login(WalletType walletType, String publicAddress,
       String domain, String uri) async {
@@ -278,11 +278,11 @@ class ParticleConnect {
   }
 
   /// Verify sign in with Ethereum/Solana locally.
-  /// 
+  ///
   /// Pass [walletType] and [publicAddress] to decide a wallet.
-  /// 
+  ///
   /// [message] is source message, comes from login.
-  /// 
+  ///
   /// [signature] is signature, comes from login.
   static Future<String> verify(WalletType walletType, String publicAddress,
       String message, String signature) async {
