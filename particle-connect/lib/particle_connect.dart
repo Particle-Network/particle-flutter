ef54616bacacccf7b94b7828412cd02b10b6032e
Houn <xuxihong@minijoy.work>
2023-03-27 11:09:32 +0800

    fix

--- particle-connect/lib/particle_connect.dart
+++ particle-connect/lib/particle_connect.dart
@@ -349,15 +349,14 @@ class ParticleConnect {
         }));
   }
 
-  /// Add ethereum chain, only support metamask
+  /// get walletReadyState
   /// Pass [walletType] to decide a wallet.
   /// android supported wallet:MetaMask,Rainbow,Trust,ImToken,BitKeep,MathWallet,TokenPocket,Omni,Zerion,Coin98,Bitpie,ZenGo,Alpha,TTWallet
   static Future<WalletReadyState> walletReadyState(
       WalletType walletType) async {
-      String readyState = await _channel.invokeMethod(
-          'walletReadyState', jsonEncode({"wallet_type": walletType.name}));
-      return WalletReadyState.values.byName(readyState);
-
+    String readyState = await _channel.invokeMethod(
+        'walletReadyState', jsonEncode({"wallet_type": walletType.name}));
+    return WalletReadyState.values.byName(readyState);
   }
 
   /// reconnect wallet connect wallet, only support iOS
