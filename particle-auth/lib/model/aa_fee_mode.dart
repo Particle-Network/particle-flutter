2560923b13e011a9c7e1090ec52b95a024debeb1
shuoshubao <shuoshubao@gmail.com>
2023-09-18 15:53:23 +0800

    docs

--- particle-auth/lib/model/aa_fee_mode.dart
+++ particle-auth/lib/model/aa_fee_mode.dart
@@ -7,7 +7,7 @@ class BiconomyFeeMode {
   /// select native for fee
   /// There are two use cases.
   /// pass null as wholeFeeQuote, to send a user paid transaction, use native as gas fee.
-  /// specify wholeFeeQuote, that you can get from particle_biconomy rpcGetFeeQuotes, to send a user paid transaction, use native as gas fee.
+  /// specify wholeFeeQuote, that you can get from particle_aa rpcGetFeeQuotes, to send a user paid transaction, use native as gas fee.
   BiconomyFeeMode.native(this.wholeFeeQuote)
       : option = 'native',
         feeQuote = null,
@@ -16,14 +16,14 @@ class BiconomyFeeMode {
   /// gasless
   /// There are two use cases.
   /// pass null as wholeFeeQuote, to send a gasless transaction.
-  /// specify wholeFeeQuote, that you can get from particle_biconomy rpcGetFeeQuotes, to send a gasless transaction
+  /// specify wholeFeeQuote, that you can get from particle_aa rpcGetFeeQuotes, to send a gasless transaction
   BiconomyFeeMode.gasless(this.wholeFeeQuote)
       : option = 'gasless',
         feeQuote = null,
         tokenPaymasterAddress = null;
 
   /// select token for fee
-  /// specify feeQuote and tokenPaymasterAddress, that you can get from particle_biconomy rpcGetFeeQuotes, to send a user paid transaction, use token as gas fee.
+  /// specify feeQuote and tokenPaymasterAddress, that you can get from particle_aa rpcGetFeeQuotes, to send a user paid transaction, use token as gas fee.
   BiconomyFeeMode.token(this.feeQuote, this.tokenPaymasterAddress)
       : option = 'token',
         wholeFeeQuote = null;
